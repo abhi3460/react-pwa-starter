11777fa6e18accf6d66b97499319c47c997ac294
abhishek-7span <abhishek@7span.com>
2023-12-21 12:20:09 +0530

    [WOHOO] React PWA Starter is onboard now.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Hello() {
+  return (
+    <div className="wrapper">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default Hello;
